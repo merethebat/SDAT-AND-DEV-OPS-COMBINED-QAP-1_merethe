bd022c3e40c483d7b2cd016932be384bb2cda354
merethebat <merethe.batino@keyin.com>
2026-05-29 03:35:53 +0000

    new commit

--- EvenBookingSystem/src/main/java/com/keyin/Event.java
+++ EvenBookingSystem/src/main/java/com/keyin/Event.java
@@ -4,13 +4,21 @@
 
 package com.keyin;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Event {
     private String name;
     private String date;
+    private int capacity;
+    private List<String> attendees;
+    public String getCapacity;
 
     public Event(String name, String date) {
         this.name = name;
         this.date = date;
+        this.capacity = 100; // Default capacity
+        attendees = new ArrayList<>();
     }
 
     public String getName() {
@@ -20,4 +28,40 @@ public class Event {
     public String getDate() {
         return date;
     }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    //register attendees
+    public String registerAttendee(String attendeeName) {
+        if (attendees.size() < capacity) {
+            attendees.add(attendeeName);
+            return "Attendee " + attendeeName + " registered successfully.";
+        } else {
+            return "Event is full. Cannot register. " + attendeeName + ".";
+        }
+    }
+
+    //cancel registration
+    public String cancelRegistration(String attendeeName){
+        if (attendees.contains(attendeeName)){
+            attendees.remove(attendeeName);
+            return name + " Registration cancelled.";
+        }
+        return attendeeName + " not found.";
+    }
+
+    //display attendees
+    public void displayAttendees(){
+        System.out.println("Attendees for" + name + ":");
+        if (attendees.isEmpty()){
+            System.out.println("No attendees registered.");
+            return;
+        }
+        for (String person : attendees){
+            System.out.println("-" + person);
+        }
+    }
 }
+
